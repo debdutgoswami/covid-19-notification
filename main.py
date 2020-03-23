61ff7e90609b951700ac85d9d38be573d2ee9c97
Debdut Goswami <debdutgoswami@gmail.com>
2020-03-24 03:56:34 +0530

    ADD graph feature

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# imports
 import requests, json, os, time
 # email
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 # beautifulsoup4
 from bs4 import BeautifulSoup
 # enviornment variable
@@ -13,6 +15,8 @@ import firebase_admin
 from firebase_admin import db, credentials
 # matplotlib
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from matplotlib.lines import Line2D
 # numpy
 import numpy as np
 
@@ -92,27 +96,27 @@ def check(_json) -> str:
             if _prev["Dth"] != _cur["Dth"]:
                 cur_msg += f"Death case has changed from {_prev['In'].strip()} to {_cur['In'].strip()}\t"
             if len(cur_msg):
-                _msg += f"\r\nIn {state},\nNo. of " + cur_msg
+                _msg += f"<br>In {state},\nNo. of " + cur_msg
         except KeyError:
             #  for addition of new state
-            _msg += f"\nNew state {state} have {_update[state]['In'].strip()} Indian case, {_update[state]['Fr'].strip()} Foreign case, {_update[state]['Cur']} cured and {_update[state]['Dth']} death.\n"
+            _msg += f"<br>New state {state} have {_update[state]['In'].strip()} Indian case, {_update[state]['Fr'].strip()} Foreign case, {_update[state]['Cur']} cured and {_update[state]['Dth']} death.<br>"
 
     if change==False:
         try:
             # count of the total cases in India
             _total = rows[len(rows)-1].find_all('td')
             _totIN, _totFR = _total[1].text, _total[2].text
-            total_msg = f"\r\nThe total no. of cases in India are {int(_totIN.strip())+int(_totFR.strip())} (including Foreign Nationality) having {_total[3].text.strip()} cured and {_total[4].text.strip()} deaths."
+            total_msg = f"<br><br>The total no. of cases in India are {int(_totIN.strip())+int(_totFR.strip())} (including Foreign Nationality) having {_total[3].text.strip()} cured and {_total[4].text.strip()} deaths."
         except ValueError:
             # count of the total cases in India
             _total = rows[len(rows)-1].find_all('td')
             _totIN, _totFR = _total[1].text, _total[2].text
-            total_msg = f"\r\nThe total no. of cases in India are {int(_totIN[:len(_totIN)-2].strip())+int(_totFR[:len(_totFR)-2].strip())} (including Foreign Nationality) having {_total[3].text.strip()} cured and {_total[4].text.strip()} deaths."
+            total_msg = f"<br><br>The total no. of cases in India are {int(_totIN[:len(_totIN)-2].strip())+int(_totFR[:len(_totFR)-2].strip())} (including Foreign Nationality) having {_total[3].text.strip()} cured and {_total[4].text.strip()} deaths."
     else:
         # count of the total cases in India
         _total = rows[len(rows)-2].find_all('td')
         _totIN, _totFR = _total[1].text, _total[2].text
-        total_msg = f"\r\nThe total no. of cases in India are {int(_totIN[:len(_totIN)-2].strip())+int(_totFR[:len(_totFR)-2].strip())} (including Foreign Nationality) having {_total[3].text.strip()} cured and {_total[4].text.strip()} deaths."
+        total_msg = f"<br><br>The total no. of cases in India are {int(_totIN[:len(_totIN)-2].strip())+int(_totFR[:len(_totFR)-2].strip())} (including Foreign Nationality) having {_total[3].text.strip()} cured and {_total[4].text.strip()} deaths."
 
     # if there is no change then there is no point in sending notification
     if len(_msg):
@@ -120,24 +124,30 @@ def check(_json) -> str:
             json.dump(_update, f)
 
         _msg += total_msg
-        _msg = "Hi,\r\nThere has been an update in the number of cases reported for COVID-19.\r\n" + _msg
+        _msg = "Hi,<br>There has been an update in the number of cases reported for COVID-19.<br><br>" + _msg
 
         return _msg
 
     return None
 
 def create_chart():
+    custom_lines = [
+        Line2D([0], [0], color='#0000FF', lw=4),
+        Line2D([0], [0], color='#00FF00', lw=4),
+        Line2D([0], [0], color='#FF0000', lw=4)
+    ]
     x=np.arange(len(graph['states']))
-    ax = plt.subplot(1,1,1)
     w = 0.3
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(left=0.08, bottom=0.35, right=0.96, top=0.88)
     plt.xticks(x, graph['states'], rotation='vertical')
-    ax.bar(x-w, graph['total'], width=w, color='b', align='center')
-    ax.bar(x, graph['cured'], width=w, color='g', align='center')
-    ax.bar(x+w, graph['death'], width=w, color='r', align='center')
-    plt.xlabel('States')
-    plt.ylabel('Person')
-    plt.autoscale(enable=False)
-    plt.show()
+    ax.bar(x-w, graph['total'], width=w, color='#0000FF', align='center')
+    ax.bar(x, graph['cured'], width=w, color='#00FF00', align='center')
+    ax.bar(x+w, graph['death'], width=w, color='#FF0000', align='center')
+    ax.legend(custom_lines, ['Total (India + Foreign)', 'Cured', 'Death'])
+    plt.title('COVID-19 INDIA OUTBREAK')
+    fig.savefig('update')
+    plt.close(fig=fig)
 
 
 def send_notification(EMAIL_BODY: str):
@@ -157,7 +167,12 @@ def send_notification(EMAIL_BODY: str):
     msg['From'] = fromaddr
     msg['To'] = fromaddr
     msg['Subject'] = "IMPORTANT: Updates on COVID-19 Cases in India"
-    msg.attach(MIMEText(EMAIL_BODY))
+    with open('update.png', 'rb') as f:
+        msgImage = MIMEImage(f.read())
+    msgImage.add_header('Content-ID', '<image1')
+    _html = '<p>'+EMAIL_BODY+'</p><br><img src="cid:image1">'
+    msg.attach(MIMEText(EMAIL_BODY, 'html'))
+    msg.attach(msgImage)
 
     # didn't add masg['To'] because anything we put in msg is visible to everyone so the everyones' email will be visible
     for toaddr in emails:
@@ -177,8 +192,9 @@ if __name__ == "__main__":
         emails = ['debdutgoswami@gmail.com']
 
         EMAIL_BODY = check(_json)
-        create_chart()
-        # if EMAIL_BODY:
-        #     send_notification(EMAIL_BODY)
+
+        if EMAIL_BODY:
+            create_chart()
+            send_notification(EMAIL_BODY)
 
         time.sleep(300)
